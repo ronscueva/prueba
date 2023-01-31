3bd1dc37c86dc4db7aed3215eb63167a2b9aa5d0
Dennisevangelista <dennisevangelista300@gmail.com>
2023-01-30 19:34:56 -0500

    borrado

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  define( 'DB_HOST', 'a2plcpnl0863.prod.iad2.secureserver.net' );          // Set database host
-  define( 'DB_USER', 'bd_sistema' );             // Set database user
-  define( 'DB_PASS', '%Sistemas0rb1n3t@' );             // Set database password
-  define( 'DB_NAME', 'siscontrol' );        // Set database name
-
-?>
